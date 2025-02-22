a932580b3cee4c525c21ef22d3f3a3af24c05289
Pierre Bastos <pierre_brun0@hotmail.com>
2025-02-22 00:37:25 -0300

    continuação exercício 1

--- Projetos/Exemplo 1/Program.cs
+++ Projetos/Exemplo 1/Program.cs
@@ -1,5 +1,27 @@
 ﻿using Exemplo_1.Models;
 
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+string apresentacao = "Olá, seja bem vindo";
+
+int quantidade = 1;
+
+double altura = 1.80;
+
+decimal preco = 1.80M;
+
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+Console.WriteLine("Valor da variável preco: " + preco);
+Console.WriteLine("Valor da variável condicao: " + condicao);
+
+
+
 Pessoa p = new Pessoa();
 
 p.Nome = "Buta";
